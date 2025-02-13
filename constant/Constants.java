9dcdca4a70e00dff1342e96648d1f19ff899a908
Steven Tjoa <steventjoa89@gmail.com>
2025-02-14 02:09:45 +0700

    Remove backedup files.

--- constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.constant;
-
-public class Constants {
-    public static final long ACCESS_TOKEN_EXPIRATION = 3600000; // 1 hour
-    public static final long REFRESH_TOKEN_EXPIRATION = 604800000; // 7 days
-}
--- service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface JwtService {
-    String generateToken(String name);
-    String validateToken(String token);
-}
